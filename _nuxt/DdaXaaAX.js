5f34bdb84e9816f1a26b052dfc4b6773c7ba714e
miguelrk <miguelromerokaram@gmail.com>
2025-06-10 16:10:33 +0000

    Deploying to gh-pages from @ netzo/docs@823fba7f18b7b1178bbd93c38531d218ef9bb690 🚀

---
+++ _nuxt/DdaXaaAX.js
@@ -0,0 +1,1 @@
+import{r as t}from"./uD9TIzwr.js";import e from"./C-uX18GK.js";import{z as n,H as s,i as a,w as i,W as p}from"./BJbBcVZS.js";import"./C2bHYaEW.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
